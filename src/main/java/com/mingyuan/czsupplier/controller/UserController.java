1d64df7bd56992e2fb4920996526797bfdd6bfe3
Admin <wuj13@mingyuanyun.com>
2020-08-08 01:31:29 +0800

    modify

--- src/main/java/com/mingyuan/czsupplier/controller/UserController.java
+++ src/main/java/com/mingyuan/czsupplier/controller/UserController.java
@@ -2,17 +2,24 @@ package com.mingyuan.czsupplier.controller;
 
 import com.mingyuan.czsupplier.vo.LoginVo;
 import com.mingyuan.czsupplier.vo.RequestBean;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
 
     @PostMapping("login")
-    public void login(@RequestBody RequestBean<LoginVo> requestBean){
-        System.out.println(requestBean);
+    public void login(@Valid @RequestBody RequestBean<LoginVo> requestBean, BindingResult result){
+        if(result.hasErrors()){
+            result.getFieldErrors().forEach(item ->{
+                System.out.print(item.getDefaultMessage());
+            });
+        }
     }
 }
--- src/main/java/com/mingyuan/czsupplier/vo/RequestBean.java
+++ src/main/java/com/mingyuan/czsupplier/vo/RequestBean.java
@@ -2,6 +2,8 @@ package com.mingyuan.czsupplier.vo;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+
 @Data
 public class RequestBean<T>{
     private T data;
